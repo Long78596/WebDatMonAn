6cb3bd65693cfe37ddd87f3a74fa04c126a82079
DaziiHP <lonis0405@gmail.com>
2024-06-17 19:53:40 +0700

    model phân quyền, shipper

---
+++ Models/PhanQuyenModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace WebDatMonAn.Models
+{
+    public class PhanQuyenModel
+    {
+        public int MaPQ { get; set; }
+
+        public string TenQuyen { get; set; }
+
+        public string MoTa { get; set; }
+    }
+}
---
+++ Models/ShipperModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace WebDatMonAn.Models
+{
+    public class ShipperModel
+    {
+        public int MaShip { get; set; }
+
+        public string Ten { get; set; }
+
+        public string DienThoai { get; set; }
+
+        public string CongTy { get; set; }
+
+        public DateTime NgayLayDon { get; set; }
+    }
+}
